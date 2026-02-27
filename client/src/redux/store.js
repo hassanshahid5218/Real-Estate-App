314d39d808c76efc559a5bcd9004305ec43f1b7c
Muhammad hassan <hassanshahid5218@gmail.com>
2026-02-27 15:53:57 +0500

    add redux-persist

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,7 +1,29 @@
-import { configureStore } from '@reduxjs/toolkit'
-import  userReducer  from './user/user slice'
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
+import  userReducer  from './user/user slice.js'
+import { persistReducer, persistStore } from 'redux-persist'
+// import storage from 'redux-persist/lib/storage'
+const storage = {
+  getItem: (key) => Promise.resolve(localStorage.getItem(key)),
+  setItem: (key, value) => Promise.resolve(localStorage.setItem(key, value)),
+  removeItem: (key) => Promise.resolve(localStorage.removeItem(key)),
+};
+
+
+const rootReducer=combineReducers({user:userReducer})
+ 
+const persistconfig={
+   key:'root',
+   storage,
+   version:1
+
+}
+
+
+const persisReducer=persistReducer(persistconfig,rootReducer)
+
+
 export const store = configureStore({
-  reducer: {user:userReducer},
+  reducer: persisReducer,
   middleware:(getDefaultmiddleware)=>
     getDefaultmiddleware({
         serializableCheck:false
@@ -12,3 +34,5 @@ export const store = configureStore({
 // export type RootState = ReturnType<typeof store.getState>
 // // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 // export type AppDispatch = typeof store.dispatch
+
+export const persistor=persistStore(store);
