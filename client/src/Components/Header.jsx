b0ff957fa40105f81d3be6f227ea59feb7ad5fc8
Muhammad hassan <hassanshahid5218@gmail.com>
2026-03-23 21:20:22 +0500

    complete header search form fuctionality

--- client/src/Components/Header.jsx
+++ client/src/Components/Header.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import {FaSearch} from 'react-icons/fa'
-import { Link } from 'react-router-dom'
+import { Link ,useNavigate} from 'react-router-dom'
 import { useSelector } from 'react-redux'
 export default function Header() {
+  const Navigate=useNavigate();
   const {currentuser}=useSelector(state=>state.user)
+  const [searchTerm,setSearchTerm]=useState("")
+  const handlesubmit=(e)=>{
+        e.preventDefault();
+        const urlParams=new URLSearchParams(window.location.search);
+        urlParams.set("searchTerm",searchTerm);
+        const searchQuery=urlParams.toString();
+        Navigate(`/search?${searchQuery}`)
+  }
+  useEffect(()=>{
+    const urlParams=new URLSearchParams(location.search);
+    const searchTermFromUrl=urlParams.get('searchTerm');
+    if(searchTermFromUrl){
+      setSearchTerm(searchTermFromUrl)
+    }
+  },[location.search]);
   return (
     <div>
       <header className='bg-slate-200 shadow-md' >
@@ -14,9 +30,11 @@ export default function Header() {
             <span className='text-slate-700'>Real Estate</span>
         </h1>
         </Link>
-        <form className='bg-slate-100 p-3 rounded-lg flex items-center w-24 sm:w-65'>
-          <input type='text' placeholder='Search here' className='bg-transparent focus:outline-none w-full'/>
+        <form onSubmit={handlesubmit} className='bg-slate-100 p-3 rounded-lg flex items-center w-24 sm:w-65'>
+          <input type='text' placeholder='Search here' className='bg-transparent focus:outline-none w-full' onChange={(e)=>setSearchTerm(e.target.value)} />
+          <button>
           <FaSearch className='text-slate-500'/>
+          </button>
         </form>
         <ul className='flex gap-4'>
          <Link to="/"><li className='hidden sm:inline text-slate-700 hover:underline'>Home</li></Link>
