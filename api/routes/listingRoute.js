3b1e3ac5922554588fafc83ca30b5a0f21b09109
Muhammad hassan <hassanshahid5218@gmail.com>
2026-03-23 20:30:20 +0500

    create search api route

--- api/routes/listingRoute.js
+++ api/routes/listingRoute.js
@@ -1,11 +1,12 @@
 const exp=require('express')
 const router=exp.Router()
-const {creatingList,deleteListing,updateListing,getListing}=require('../controller/list controller.js')
+const {creatingList,deleteListing,updateListing,getListing,getListings}=require('../controller/list controller.js')
 const { verifyToken } = require('../utills/verifyuser.js')
 router.post('/create',verifyToken,creatingList)
 router.delete('/delete/:id',verifyToken,deleteListing)
 router.post('/update/:id',verifyToken,updateListing)
 router.get('/get/:id',getListing)
+router.get('/get',getListings)
 
 
 module.exports=router
